c28fdc3c6af18544da773d1cc64c1b5df0470346
Gulshabina <156685132+Gulshabina@users.noreply.github.com>
2024-12-07 17:01:36 +0500

    Add files via upload

---
+++ node.js
@@ -0,0 +1,23 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+  {/* // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apikey: "AIzaSyD6CziaQKE2kjb9ufRc4iR9HWjEUKrZJT0",
+    authDomain: "project-90a52.firebaseapp.com",
+    projectId: "project-90a52",
+    storageBucket: "project-90a52.firebasestorage.app",
+    messagingSenderId: "956616641240",
+    appId: "1:956616641240:web:0775e4c607986c69cde5ad",
+    measurementId: "G-1CF5NPZDC7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script> */}
